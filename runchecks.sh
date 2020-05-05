3af9c5e812f26b382a6b26d750fa1e50cb666253
Alice Harpole <aliceharpole@gmail.com>
2020-05-05 11:20:38 -0400

    add cd back in

--- runchecks.sh
+++ runchecks.sh
@@ -12,7 +12,7 @@ bear make $INPUT_MAKE_OPTIONS
 clang-tidy --version
 python3 run-clang-tidy.py -header-filter=$INPUT_HEADER_FILTER -ignore-files=$INPUT_IGNORE_FILES -j 2 -checks=$INPUT_CHECKS > $GITHUB_WORKSPACE/clang-tidy-report.txt
 
-# cd $REPO_PATH
+cd $REPO_PATH
 # find . -regex '.*\.\(cpp\|h\|H\)' | xargs clang-format --style=llvm -i 2>&1 | tee $GITHUB_WORKSPACE/clang-format-report.txt
 
 cppcheck --enable=all --force --std=c++11 --language=c++ --output-file=$GITHUB_WORKSPACE/cppcheck-report.txt *
