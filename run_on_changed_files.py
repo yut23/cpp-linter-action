edd6a1a882e407357794128bd1b9b8fe7a6ea519
Alice Harpole <aliceharpole@gmail.com>
2020-08-03 13:12:43 -0400

    add prints

--- run_on_changed_files.py
+++ run_on_changed_files.py
@@ -23,7 +23,12 @@ def run(SHAs=None, make_options='', header_filter='',
     for prob_path, flags in problems.items():
         make_command = ['bear', 'make', f'{make_options}', flags]
 
+        print(f'make command = {make_command')
+
         with cd(f'Exec/{prob_path}'):
+
+            print(f'making in Exec/{prob_path')
+            
             _, stderr = subprocess.Popen(make_command,
                                       stdout=subprocess.PIPE,
                                       stderr=subprocess.STDOUT).communicate()
