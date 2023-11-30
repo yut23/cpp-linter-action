2e50e7c57a0b4ebc91ab863b3525aec945585527
Eric T. Johnson <eric.t.johnson@stonybrook.edu>
2023-11-29 22:12:51 -0500

    Improve cppcheck invocation
    * suppress errors from ignored files
    * avoid scanning ignored files for preprocessor variables (this greatly
      cuts down on the number of configurations checked)
    * allow passing additional options to cppcheck as action inputs (e.g.
      additional preprocessor variables to define or undefine)

--- runchecks.sh
+++ runchecks.sh
@@ -12,14 +12,30 @@ bear -- make $INPUT_MAKE_OPTIONS || exit $?
 clang-tidy --version
 python3 run-clang-tidy.py -header-filter=$INPUT_HEADER_FILTER -ignore-files=$INPUT_IGNORE_FILES -j 2 -config-file=$GITHUB_WORKSPACE/.clang-tidy > $GITHUB_WORKSPACE/clang-tidy-report.txt
 
-cd $REPO_PATH
+# extract -I directories from the compilation database into a shell array
+includes=()
+eval "$(jq -r 'map(.arguments | map(select(startswith("-I")) | ltrimstr("-I")))
+               | flatten | unique
+               | @sh "includes=( \(.) )"' compile_commands.json)"
 
-cppcheck_ignores=()
+# split on all whitespace (including newlines)
+# this avoids expanding globs
+read -ra cppcheck_args -d '' <<<"$INPUT_CPPCHECK_OPTIONS"
+
 # extract the directories to ignore from INPUT_IGNORE_FILES (split on '|')
 IFS='|' read -ra ignore_files <<<"$INPUT_IGNORE_FILES"
 for file in "${ignore_files[@]}"; do
   # after checking the source code, -i also accepts globs
-  cppcheck_ignores+=(-i "*/$file/*")
+  cppcheck_args+=(-i "*/$file/*")
+  # also suppress any errors from the ignored files
+  cppcheck_args+=("--suppress=*:*/$file/*")
+  # don't scan ignored directories for preprocessor variables
+  for include in "${includes[@]}"; do
+    if [[ $include = *"/$file/"* ]]; then
+      cppcheck_args+=("--config-exclude=$include")
+    fi
+  done
 done
 
-cppcheck --enable=all --force --std=c++11 --language=c++ --project=$INPUT_BUILD_PATH/compile_commands.json "${cppcheck_ignores[@]}" --output-file=$GITHUB_WORKSPACE/cppcheck-report.txt
+cppcheck --version
+cppcheck --enable=all --force --std=c++17 --language=c++ --project=compile_commands.json "${cppcheck_args[@]}" --output-file=$GITHUB_WORKSPACE/cppcheck-report.txt
